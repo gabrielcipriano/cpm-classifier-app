69bc523bb06bdeaf0354b4850f0d82b75e033322
Gabriel Cipriano <gabrielfcipriano@gmail.com>
2023-03-26 19:34:55 -0300

    feat: http consts

--- app/app/app.py
+++ app/app/app.py
@@ -5,14 +5,17 @@ from cpm_classifier import get_cpm_classifier
 
 app = Flask(__name__)
 
+OK = 200
+BAD_REQUEST = 400
+
 cpm_classifier = get_cpm_classifier()
 
 @app.route('/cpm-classifier/predict', methods=['POST'])
-def classifier():
+def cpm_predict():
     body = request.get_json()
 
     if (body is None) or (type(body) is not list):
-        return "Please include a list of objects in JSON Body", 400
+        return "Please include a list of objects in JSON Body", BAD_REQUEST
 
     model_inputs, errors = cpm_classifier.parse_samples(body)
     
@@ -20,6 +23,6 @@ def classifier():
 
     response = parse_model_response(predicts, errors)
 
-    return jsonify(response), 200
+    return jsonify(response), OK
 
 app.run(debug=True)
